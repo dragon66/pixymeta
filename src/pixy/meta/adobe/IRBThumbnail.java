a7415dac65f20cff596aee0588e3d2bc32e3050b
dragon66 <yuwen_66@yahoo.com>
2015-04-27 14:29:30 -0400

    Added copy constructor

--- src/pixy/meta/adobe/IRBThumbnail.java
+++ src/pixy/meta/adobe/IRBThumbnail.java
@@ -13,6 +13,7 @@
  *
  * Who   Date       Description
  * ====  =========  ===========================================================
+ * WY    27Apr2015  Added copy constructor
  * WY    13Apr2015  Moved related code to ThumbnailResource
  * WY    10Apr2015  Implemented base class Thumbnail abstract method write()
  * WY    13Mar2015  Initial creation for IRBReader to encapsulate IRB thumbnail
@@ -29,7 +30,6 @@ import cafe.image.ImageParam;
 import cafe.image.ImageType;
 import cafe.image.options.JPEGOptions;
 import cafe.image.writer.ImageWriter;
-
 import pixy.meta.Thumbnail;
 
 /** 
@@ -48,6 +48,14 @@ public class IRBThumbnail extends Thumbnail {
 	
 	public IRBThumbnail(int width, int height, int dataType, byte[] compressedThumbnail) {
 		super(width, height, dataType, compressedThumbnail);
+	}
+	
+	public IRBThumbnail(IRBThumbnail other) { // Copy constructor
+		this.dataType = other.dataType;
+		this.height = other.height;
+		this.width = other.width;
+		this.thumbnail = other.thumbnail;
+		this.compressedThumbnail = other.compressedThumbnail;
 	}
 
 	@Override
