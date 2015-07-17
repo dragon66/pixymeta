ea63d743540eff0f2ece5f5084a84d3f1d20dea1
dragon66 <yuwen_66@yahoo.com>
2015-07-16 23:30:45 -0400

    Added two new constructors to IPTCDataSet

--- src/pixy/meta/iptc/IPTCDataSet.java
+++ src/pixy/meta/iptc/IPTCDataSet.java
@@ -13,6 +13,7 @@
  *
  * Who   Date       Description
  * ====  =========  =================================================
+ * WY    16Jul2015  Added two new constructors for IPTCApplicationTag
  * WY    13Mar2015  Initial creation
  */
 
@@ -67,6 +68,14 @@ public class IPTCDataSet {
 		this(tag, value.getBytes());
 	}
 	
+	public IPTCDataSet(IPTCApplicationTag appTag, byte[] data) {
+		this(appTag.getTag(), data);
+	}
+	
+	public IPTCDataSet(IPTCApplicationTag appTag, String value) {
+		this(appTag.getTag(), value);
+	}
+	
 	public IPTCDataSet(IPTCRecord record, int tag, byte[] data) {
 		this(record.getRecordNumber(), tag, data.length, data, 0);
 	}
@@ -99,6 +108,34 @@ public class IPTCDataSet {
 		return true;
 	}
 	
+	public byte[] getData() {
+		return ArrayUtils.subArray(data, offset, size);
+	}
+	
+	public String getDataAsString() {
+		return tagEnum.getDataAsString(getData());
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getRecordNumber() {
+		return recordNumber;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getTag() {
+		return tag;
+	}
+	
+	public IPTCTag getTagEnum() {
+		return tagEnum;
+	}
+	
 	private String getTagName() {
 		switch(IPTCRecord.fromRecordNumber(recordNumber)) {
 			case APPLICATION:
@@ -127,34 +164,6 @@ public class IPTCDataSet {
 		}
 		
 		return tagEnum.getName();
-	}
-	
-	public String getDataAsString() {
-		return tagEnum.getDataAsString(getData());
-	}
-	
-	public byte[] getData() {
-		return ArrayUtils.subArray(data, offset, size);
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getRecordNumber() {
-		return recordNumber;
-	}
-	
-	public int getSize() {
-		return size;
-	}
-	
-	public int getTag() {
-		return tag;
-	}
-	
-	public IPTCTag getTagEnum() {
-		return tagEnum;
 	}
 	
 	@Override
