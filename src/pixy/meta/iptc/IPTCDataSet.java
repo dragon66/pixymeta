ff6eb094f6611b8099868747fef8c1cbd84a59da
dragon66 <yuwen_66@yahoo.com>
2016-08-16 20:48:52 -0400

    Add IPTC Unicode string support

--- src/pixy/meta/iptc/IPTCDataSet.java
+++ src/pixy/meta/iptc/IPTCDataSet.java
@@ -13,6 +13,7 @@
  *
  * Who   Date       Description
  * ====  =========  =================================================
+ * WY    16Aug2016  Added support for Unicode string data
  * WY    16Jul2015  Added two new constructors for IPTCApplicationTag
  * WY    13Mar2015  Initial creation
  */
@@ -21,6 +22,7 @@ package pixy.meta.iptc;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 import org.slf4j.Logger;
@@ -51,6 +53,14 @@ public class IPTCDataSet {
 	// Obtain a logger instance
 	private static final Logger LOGGER = LoggerFactory.getLogger(IPTCDataSet.class);
 	
+	private static byte[] getBytes(String str) {
+		try {
+			return str.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Unsupported encoding UTF-8");
+		}
+	}
+	
 	public IPTCDataSet(int tag, byte[] data) {
 		this(IPTCRecord.APPLICATION, tag, data);
 	}
@@ -65,7 +75,7 @@ public class IPTCDataSet {
 	}
 	
 	public IPTCDataSet(int tag, String value) {
-		this(tag, value.getBytes());
+		this(tag, IPTCDataSet.getBytes(value));
 	}
 	
 	public IPTCDataSet(IPTCApplicationTag appTag, byte[] data) {
@@ -81,7 +91,7 @@ public class IPTCDataSet {
 	}
 	
 	public IPTCDataSet(IPTCRecord record, int tag, String value) {
-		this(record, tag, value.getBytes());
+		this(record, tag, IPTCDataSet.getBytes(value));
 	}
 	
 	public boolean allowMultiple() {
